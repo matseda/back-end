e022205198d33480eea27485ac93476b845b21f0
Mats Elias Davidsen <MEDA@statoil.com>
2018-06-26 14:43:03 +0200

    added temp array

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var temp = [["Oslo",31],["Fredrikstad",27],["Trondheim",23]]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'FFK' });
+  res.render('index', { title: temp[0][0] });
 });
 
 module.exports = router;
