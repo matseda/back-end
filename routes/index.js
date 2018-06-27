ba2b84bb4c82b661d08b11fb8d5674ecaaa12cf0
Mats Elias Davidsen <MEDA@statoil.com>
2018-06-27 09:05:49 +0200

    t

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var temp = [["Oslo",31],["Fredrikstad",27],["Trondheim",23]]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: temp[0]});
+  res.sendFile(path.join(__dirname + "/client/public/index.html"))
 });
 
 module.exports = router;
