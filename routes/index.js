7e7c5f9afd68e52c1888f40378f521d60ccca48e
Mats Elias Davidsen <MEDA@statoil.com>
2018-06-28 13:33:47 +0200

    Fixed paths

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var temp = [["Oslo",31],["Fredrikstad",27],["Trondheim",23]]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname + "/client/public/index.html"))
+  res.send('')
 });
 
 module.exports = router;
