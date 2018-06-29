45e9542697b3372a644f1e98b7494780bcf2d9ca
Mats Elias Davidsen <MEDA@statoil.com>
2018-06-29 10:45:17 +0200

    cors

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var createError = require('http-errors');
 var express = require('express');
-//var cors = require('cors');
+var cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -12,14 +12,14 @@ var app = express();
 
 // view engine setup
 /*app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');*/
+app.set('view engine', 'jade');
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
-});
+});*/
 
-//app.use(cors())
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var temp = [["Oslo",31],["Fredrikstad",27],["Trondheim",23]]
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
